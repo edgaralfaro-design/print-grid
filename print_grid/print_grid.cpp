229c95be322da667acc7b9ec94081a33f359a649
Edgar Alfaro <edgar.alfaro@sjsu.edu>
2026-04-01 13:10:22 -0700

    Fix repo structure

---
+++ print_grid/print_grid.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "Your Fleet\n";
+
+    std::cout << "0 1 2 3 4 5 6 7 8 9 10\n";
+
+    std::cout << "A \n";
+    std::cout << "B \n";
+    std::cout << "C \n";
+    std::cout << "D \n";
+    std::cout << "E \n";
+    std::cout << "F \n";
+    std::cout << "G \n";
+    std::cout << "H \n";
+    std::cout << "I \n";
+    std::cout << "J \n";
+
+    std::cout << "Enemy Waters\n";
+
+    std::cout << "0 1 2 3 4 5 6 7 8 9 10\n";
+    std::cout << "A \n";
+    std::cout << "B \n";
+    std::cout << "C \n";
+    std::cout << "D \n";
+    std::cout << "E \n";
+    std::cout << "F \n";
+    std::cout << "G \n";
+    std::cout << "H \n";
+    std::cout << "I \n";
+    std::cout << "J \n";
+
+    return 0;
+}
